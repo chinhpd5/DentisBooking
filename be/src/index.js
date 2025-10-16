097b98fa058a06bc3f0265702c0f023446317096
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-10-16 22:43:06 +0700

    base

---
+++ be/src/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+// kết nối cơ sở dữ liệu
+mongoose.connect(process.env.CONNECT_MONGODB_URL)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB:', err));
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.static(path.join(__dirname,'uploads')));
+
+app.get('/',(request, response)=>{
+  return response.send("Hello world")
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`Server is running on port http://localhost:${port}`);
+});
